e286cce08db531807221506e11626b8deb38bd9f
M_R <mr2837465@gmail.com>
2022-04-13 08:59:12 +0200

    add data to local storage, make showData a dynamic function, make fucntion changeBorderColor to change color every time

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,21 +2,36 @@ let list = document.querySelector(".app__list");
 let textInput = document.querySelector(".add-book");
 let clearBtn = document.querySelector(".clear");
 
-// Data will be saved here
-let dataText = [
-  "The Hunger Games",
-  "A Little Princess",
-  "Isolated",
-  "Moon Knight",
-];
+// ================= Data will be saved here =================
+let dataText;
 
-// create elements
-function showData() {
+// ================= Check LocalStorage =================
+if (localStorage.data) {
+  dataText = JSON.parse(localStorage.data);
+} else {
+  dataText = [
+    "The Hunger Games",
+    "A Little Princess",
+    "Isolated",
+    "Moon Knight",
+  ];
+
+  localStorage.data = JSON.stringify(dataText);
+}
+
+// to save dataMatch here for comparison
+let temp;
+// ================= create elements =================
+function showData(dataRender) {
+  if (dataRender !== temp) {
+    dataRender = JSON.parse(localStorage.data);
+  }
+
   list.innerHTML = "";
-  for (let i = 0; i < dataText.length; i++) {
+  for (let i = 0; i < dataRender.length; i++) {
     list.innerHTML += `
         <li>
-            <span>${dataText[i]}</span>
+            <span>${dataRender[i]}</span>
             <button onclick="deleteItem(${i})">delete</button>
         </li>
       `;
@@ -28,11 +43,14 @@ function showData() {
     clearBtn.innerHTML = "Clear: " + dataText.length;
     clearBtn.style.cursor = "pointer";
   }
-}
-// show data on reload
-showData();
 
-// Add book to list
+  // Change Border Color
+  changeBorderColor();
+}
+// ================= show data on reload =================
+showData(dataText);
+
+// ================= Add book to list =================
 function addBook(e, form) {
   e.preventDefault();
 
@@ -41,77 +59,81 @@ function addBook(e, form) {
   }
 
   dataText.push(textInput.value);
+  localStorage.data = JSON.stringify(dataText);
 
-  showData();
+  showData(dataText);
 
-  //   form.reset();
+  // form.reset();
   textInput.value = "";
 }
 
-// Delete one item
+// ================= Delete one item =================
 function deleteItem(e) {
   dataText.splice(e, 1);
-  showData();
+  localStorage.data = JSON.stringify(dataText);
+
+  showData(dataText);
 }
 
-// Search items
+// ================= Search items =================
 function search(e) {
   let lowerChar = e.value.toLowerCase();
+  let dataTextMatch = [];
 
   list.innerHTML = "";
   for (let i = 0; i < dataText.length; i++) {
     if (dataText[i].toLocaleLowerCase().includes(lowerChar)) {
-      list.innerHTML += `
-          <li onmouseenter="colorBorderDefault(${this})">
-          <span>${dataText[i]}</span>
-          <button onclick="deleteItem(${i})">delete</button>
-          </li>
-        `;
+      dataTextMatch.push(dataText[i]);
     }
   }
+
+  temp = dataTextMatch;
+  return showData(dataTextMatch);
 }
 
-// form search
+// ================= form search =================
 function handelSubmit(e) {
   e.preventDefault();
 }
 
-// Delete all items
+// ================= Delete all items =================
 function clearAll() {
   dataText = [];
+  localStorage.data = JSON.stringify(dataText);
   list.innerHTML = "";
   clearBtn.style.transform = "translateY(50px)";
   clearBtn.style.cursor = "default";
 }
 
-// Change border of colors
-let li = document.querySelectorAll(".app__list li");
+// ================= Change border of colors =================
+function changeBorderColor() {
+  let li = document.querySelectorAll(".app__list li");
 
-const colors = [
-  "#40486D",
-  "#1482b5",
-  "#009868",
-  "#808000",
-  "#C19A6B",
-  "#540D6E",
-  "#FFD23F",
-  "#000",
-];
+  const colors = [
+    "#40486D",
+    "#1482b5",
+    "#009868",
+    "#808000",
+    "#C19A6B",
+    "#540D6E",
+    "#FFD23F",
+    "#000",
+  ];
 
-localStorage.colors = JSON.stringify(colors);
+  localStorage.colors = JSON.stringify(colors);
 
-const colorsStorage = JSON.parse(localStorage.colors);
+  const colorsStorage = JSON.parse(localStorage.colors);
 
-if (!localStorage.color || localStorage.color >= colors.length - 1) {
-  localStorage.color = -1;
-}
+  if (!localStorage.color || localStorage.color >= colors.length - 1) {
+    localStorage.color = -1;
+  }
 
-window.onload = () => {
-  localStorage.color++;
+  window.onload = () => {
+    localStorage.color++;
+  };
 
-  li.forEach((e) => (e.style.borderColor = colors[localStorage.color]));
-};
-
-for (let i = 0; i < colorsS.length; i++) {
-  li.forEach((e) => () => {});
+  li.forEach((e) => {
+    e.style.borderColor = colors[localStorage.color];
+    localStorage.color++;
+  });
 }
